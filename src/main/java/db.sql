e656e38b00822c52f32798b097ab074870d21c6d
Samuel French <samfrench@gmail.com>
2015-05-02 15:39:51 -0500

    fixing db

--- src/main/java/db.sql
+++ src/main/java/db.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `repo` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `repo`;
--- MySQL dump 10.13  Distrib 5.6.19, for linux-glibc2.5 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.19, for osx10.7 (i386)
 --
 -- Host: localhost    Database: repo
 -- ------------------------------------------------------
--- Server version	5.6.24
+-- Server version	5.6.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,21 +25,14 @@ DROP TABLE IF EXISTS `daily_historical`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `daily_historical` (
-  `ticker` varchar(10) DEFAULT NULL,
-  `timestamp_load` timestamp NULL DEFAULT NULL,
-  `date` varchar(45) DEFAULT NULL,
-  `open` decimal(45,0) DEFAULT NULL,
-  `high` decimal(45,0) DEFAULT NULL,
-  `low` decimal(45,0) DEFAULT NULL,
-  `close` decimal(45,0) DEFAULT NULL,
-  `volume` bigint(255) DEFAULT NULL,
-  `adj_close` decimal(45,0) DEFAULT NULL,
-  `daily_historical_id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`daily_historical_id`),
-  UNIQUE KEY `daily_historical_id_UNIQUE` (`daily_historical_id`),
-  UNIQUE KEY `UNIQUE` (`ticker`,`date`),
-  CONSTRAINT `daily_ticker_fk` FOREIGN KEY (`ticker`) REFERENCES `nasdaq_ticker` (`ticker`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `date` varchar(11) DEFAULT NULL,
+  `open` double DEFAULT NULL,
+  `high` double DEFAULT NULL,
+  `low` double DEFAULT NULL,
+  `close` double DEFAULT NULL,
+  `volume` int(11) unsigned DEFAULT NULL,
+  `adjusted_volume` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +41,6 @@ CREATE TABLE `daily_historical` (
 
 LOCK TABLES `daily_historical` WRITE;
 /*!40000 ALTER TABLE `daily_historical` DISABLE KEYS */;
-INSERT INTO `daily_historical` VALUES ('AAPL','2015-05-02 05:02:36','dfs',1,5,1,4,345,534,00000000002);
 /*!40000 ALTER TABLE `daily_historical` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-02  0:29:40
+-- Dump completed on 2015-05-02 15:39:25
